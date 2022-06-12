d05d25dca367fb245f485cd09aee07989d51650e
ZeffK <54438151+ZeffK@users.noreply.github.com>
2022-06-12 23:00:18 +0530

    for deployment

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 
-import {v4 as uuidv4} from 'uuid'
+// import {v4 as uuidv4} from 'uuid'
 import{BrowserRouter as Router , Route,Routes} from 'react-router-dom'
 import AboutIconLink from './AboutIconLink'
-import FeedbackData from './data/data';
+// import FeedbackData from './data/data';
 import './App.css';
-import Component from './component'
-import Component2 from './component2';
-import FeedbackItem from './FeedbackItem';
-import Header from './header';
+// import Component from './component'
+// import Component2 from './component2';
+// import FeedbackItem from './FeedbackItem';
+// import Header from './header';
 import { useState } from 'react';
 import Feedbacklist from './Feedbacklist';
 import Feedbackstats from './FeedbackStats';
@@ -41,8 +41,8 @@ function App() {
         </Routes>
 
         
-        <Component text="this is wriiten in app.js passed to Component 1"></Component>
-        <Component2/>
+        {/* <Component text="this is wriiten in app.js passed to Component 1"></Component>
+        <Component2/> */}
         <AboutIconLink />
         
        
